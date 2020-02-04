191243aff60e550bebd418ad48fbad295092b81b
Amit <eluminous_se42@eluminoustechnologies.com>
2020-02-04 10:26:14 +0530

    home page,about us page

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require('express')
 const app = express()
  
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Home')
+})
+
+app.get('/about-us', function (req, res) {
+  res.send('About Us Page')
 })
  
-app.listen(3000)
+app.listen(3000,function(){
+	console.log('Server running on port 3000')
+})
